389183ccad104d971ab2eb0d101fc40718b30b0f
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-06-12 16:06:58 +0200

    utils file is updated with weight init

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import yaml
 import torch
+import torch.nn as nn
 import joblib
 
 
@@ -31,3 +32,14 @@ def device_init(device="cuda"):
         return torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     else:
         return torch.device("cpu")
+
+
+def weight_init(m):
+    classname = m.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
